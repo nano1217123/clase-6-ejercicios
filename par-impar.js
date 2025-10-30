7fa45dace4ab777563549b2d15307405f183d7fc
emiliano-florez-pelaez <pelaezemiliano8@gmail.com>
2025-10-29 20:47:32 -0500

    Agrego el cuarto punto de la actividad

---
+++ par-impar.js
@@ -0,0 +1,16 @@
+//par-impar
+// 4
+
+console.log('numeros pares');
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+console.log('numeros impares');
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
